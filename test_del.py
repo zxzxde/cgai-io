50e1c52e08dc39a51cb32d198501a8a72ba5714a
老张 <360014296@qq.com>
2021-07-01 21:38:17 +0800

    增加文件及文件目录删除功能

---
+++ test_del.py
@@ -0,0 +1,19 @@
+# -*- coding:utf-8 -*-
+
+from cgai_io.Delete import delfile,deldir,delall
+
+
+# #删除文件
+# path = r'\\192.168.1.248\3d\temp\a\cmd_mac.py'
+# delfile(path)
+
+
+# #删除文件夹
+# path = r'D:\BaiduNetdiskDownload\AA'
+# deldir(path,keep_dir=True)
+
+
+#无论文件或文件夹都直接删除
+# path = r'D:\BaiduNetdiskDownload\ktk_103024'
+# delall(path)
+
